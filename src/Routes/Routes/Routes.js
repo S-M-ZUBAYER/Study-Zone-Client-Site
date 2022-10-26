37ca5f903cdb05f2b74aaede41aacf3c82ca74fb
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-10-26 23:11:03 +0600

    Blog route set up and complete the design

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import About from '../../components/AboutPage/About/About';
 import LogIn from '../../components/AuthPages/LogIn/LogIn';
 import Register from '../../components/AuthPages/Register/Register';
+import Blog from '../../components/BlogPage/Blog';
+import Contact from '../../components/ContactPage/Contact';
 import Courses from '../../components/CoursesPage/Courses/Courses';
+import FAQ from '../../components/FAQPage/FAQ/FAQ';
 import Heading from '../../components/HomePage/Heading/Heading';
 import Home from '../../components/HomePage/Home/Home';
 import Main from '../../components/Main/Main';
@@ -22,12 +26,12 @@ export const routes = createBrowserRouter([
             },
             {
                 path: '/home',
-                loader: () => fetch('http://localhost:5000/courses'),
+                loader: () => fetch('https://study-zone-server-site.vercel.app/courses'),
                 element: <Home></Home>
             },
             {
                 path: '/courses',
-                loader: () => fetch('http://localhost:5000/courses'),
+                loader: () => fetch('https://study-zone-server-site.vercel.app/courses'),
                 element: <Courses></Courses>
             },
             {
@@ -43,8 +47,24 @@ export const routes = createBrowserRouter([
                 element: <Heading></Heading>
             },
             {
+                path: '/about',
+                element: <About></About>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/contact',
+                element: <Contact></Contact>
+            },
+            {
                 path: '/courses/:id',
-                loader: ({ params }) => fetch(`http://localhost:5000/courses/${params.id}`),
+                loader: ({ params }) => fetch(`https://study-zone-server-site.vercel.app/courses/${params.id}`),
                 element: <CourseDetails></CourseDetails>
             }
         ]
